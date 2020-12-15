b0b62dea0119e30ae10c434c7f9a4c7889acaab5
tsnitzer <tleesnitzer@gmail.com>
2020-12-14 18:55:39 -0600

    updated verification response

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function() {
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
-//VALIDATION
+//PILOT VALIDATION
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
          alert("All fields are required");
          event.preventDefault();
@@ -16,49 +16,30 @@ window.addEventListener("load", function() {
       } else if (isNaN(fuelLevelInput.value) == true || isNaN(cargoMassInput.value) == true) {
          alert("Fuel level and cargo mass values must be a number");
          event.preventDefault();
-      }
-      if (pilotNameInput.value && copilotNameInput.value) {
-         //event.preventDefault();
-         document.getElementById("launchStatus").innerHTML = "Shuttle ready for launch";
-         document.getElementById("launchStatus").style.color = 'green';
+      } else {
+
+         if (fuelLevelInput.value < 10000) {
+            event.preventDefault();
+            document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+            document.getElementById("launchStatus").style.color = 'red';
+            document.getElementById("faultyItems").style.visibility = 'visible';
+         } 
+         if (cargoMassInput.value > 10000) {
+            event.preventDefault();
+            document.getElementById("cargoStatus").innerHTML = "Cargo mass too high for launch";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+            document.getElementById("launchStatus").style.color = 'red';
+            document.getElementById("faultyItems").style.visibility = 'visible';
+         }
+         if (fuelLevelInput.value >= 10000 && cargoMassInput.value <= 10000) {
+            event.preventDefault();
+            document.getElementById("launchStatus").innerHTML = "Shuttle ready for launch";
+            document.getElementById("launchStatus").style.color = 'green';
+         }
+
          document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotNameInput.value} ready`;
          document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotNameInput.value} ready`;
-         document.getElementById("faultyItems").style.visibility = 'visisble';
       }
-      if (fuelLevelInput.value < 10000) {
-         event.preventDefault();
-         document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
-         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
-         document.getElementById("launchStatus").style.color = 'red';
-         document.getElementById("faultyItems").style.visibility = 'visible';
-      }
-      if (cargoMassInput.value > 10000) {
-         event.preventDefault();
-         document.getElementById("cargoStatus").innerHTML = "Cargo mass too high for launch";
-         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
-         document.getElementById("launchStatus").style.color = 'red';
-         document.getElementById("faultyItems").style.visibility = 'visible';
-      }      
    });
 });
-
-//This block of code shows how to format the HTML once you fetch some planetary JSON!
-{/* <h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}"> */}
-
-// document.getElementById("missionTarget").innerHTML = "<h2>Mission Destination</h2>
-// <ol>
-//    <li>Name: ${}</li>
-//    <li>Diameter: ${}</li>
-//    <li>Star: ${}</li>
-//    <li>Distance from Earth: ${}</li>
-//    <li>Number of Moons: ${}</li>
-// </ol>
-// <img src="${}"></img>"
